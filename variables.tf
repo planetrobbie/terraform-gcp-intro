114905996ebec390d7d0ed5875b2d8106a11a487
Sebastien Braun <sebbraun@gmail.com>
2018-08-06 10:29:17 +0200

    variable description added/updated

--- variables.tf
+++ variables.tf
@@ -1,13 +1,15 @@
 variable "region" {
+  description = "GCP region targeted"
   default = "europe-west1"
 }
 
 variable "region_zone" {
+  description = "GCP zone targeted"
   default = "europe-west1-c"
 }
 
 variable "project_name" {
-  description = "sebbraun-yet"
+  description = "GCP project targeted"
 }
 
 #variable "account_file_path" {
@@ -15,10 +17,10 @@ variable "project_name" {
 #}
 
 variable "image" {
-  description = "Google Compute Image to use"
+  description = "GCP Image to use"
 }
 
 variable "instance_type" {
-  description = "Google Compute Machine Type to use"
+  description = "GCP Machine Type to use"
   default = "f1-micro"
 }
